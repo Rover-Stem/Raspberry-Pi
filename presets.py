e4b50d42d6bc8eee8e8817381c7e2dc80b040720
Geogre177 <mittry20017@gmail.com>
2022-04-29 17:27:22 -0400

    Direction added

--- presets.py
+++ presets.py
@@ -24,101 +24,50 @@ def getDirectionLoop (rover):
 
 def obstacleAvoidance1 (rover):
 
-	rover.moveServo(0)
+	rover.moveRover("f")
 
-	rover.moveRover("f", throttle = 0.5)
-
-	angle = 1
-
-	distance = rover.measureDistance()
-
-	#print(f"Distance: {distance}")
-
-	while (distance > 10):
-
-		#print(f"Angle is {angle}")
-
-		angle += 0.05
-		angle = angle % 2
-
-		rover.moveServo(-1 + angle)
-		#print(f"Angle should be {-1 + angle}")
-
-		distance = rover.measureDistance()
-
-		#print(f"Distance: {distance}")
-
+	while (rover.measureDistance() > 25):
 		pass
 
 	rover.moveRover("s")
 
 def distanceChallenge (rover, distance):
 
-	rover.moveRover("f", throttle = 1)
-	time.sleep((distance / (67)))
-	rover.moveRover("s")
+	rover.moveDistance(distance)
 
 # assumes that 0 degrees is north
 # possibly add if elif else statments to accodmetate for angles over 180 degrees
 def directionChallenge (rover, target):
+	rover.moveRover("rr")
+	rover.sleep(target / 170)
+	rover.moveRover("f")
+	rover.sleep(5)
 
-	direction = rover.getDirection()
-	targetDirection = target
-
-	while True:
-
-		if (direction < targetDirection):
-
-			rover.moveRover("cfr")
-
-		elif (direction > targetDirection):
-
-			rover.moveRover("cfl")
-
-		else:
-
-			rover.moveRover("f")
-
-		direction = rover.getDirection()
-
-def obstacleAvoidance2 (rover, numObstacles = 1):
+def obstacleAvoidance2 (rover, numObstacles):
 
 	count = 0
-	times = 2
 
 	while True:
 
-		rover.moveServo(0)
+		direction = rover.getDirection()
+		rover.moveRover("f")
 
-		#direction = rover.getDirection(True)
-		rover.moveRover("f", throttle = 0.25)
-
-		angle = 1
-
-		while ((rover.measureDistance() > 25) or (rover.measureDistance() < 0)):
-
-			angle += 0.05
-			angle = angle % 2
-
-			rover.moveServo(-1 + angle)
+		while (rover.measureDistance() > 5):
 
 			pass
 
-		rover.moveRover("cfl", throttle = 0.5)
-		rover.moveServo(-1)
-		time.sleep(times)
+		rover.moveRover("cfl")
+		rover.moveServo(1)
+		timeStart = time.time()
+
+		while (rover.measureDistance() > 10):
+
+			pass
+
 		timeEnd = time.time()
-		rover.moveRover("cfr", throttle = 0.5)
-		time.sleep(times + 0.2)
-		rover.moveRover("cfr", throttle = 0.5)
-		time.sleep(times + 0.3)
-		rover.moveRover("cfl", throttle = 0.5)
-		time.sleep(times + 0.3)
-		#direction2 = rover.getDirection()
-
-		rover.moveRover("s")
-
-		return
+		rover.moveRover("cfr")
+		time.sleep(timeEnd - timeStart)
+		direction2 = rover.getDirection()
 
 		# TODO: Work on fixing logic
 		while True:
@@ -151,14 +100,14 @@ def parallelParking (rover, left = False):
 	if left:
 
 		rover.moveServo(-1)
-		rover.moveRover("l", throttle = 0.75)
+		rover.moveRover("l")
 
 	else:
 
 		rover.moveServo(1)
-		rover.moveRover("r", throttle = 0.75)
+		rover.moveRover("r")
 
-	while (rover.measureDistance(True) > 35):
+	while (rover.measureDistance() > 3):
 
 		pass
 
@@ -166,27 +115,21 @@ def parallelParking (rover, left = False):
 
 def stayInYourLane (rover):
 
-	rover.moveRover("f", throttle = 0.5)
-
 	while True:
 
-		ir1 = rover.pingIR1()
+		diff = np.round(getPath(), 1)
 
-		if not(ir1):
+		if (diff == 0):
 
-			rover.moveRover("cfr", throttle = 0.5)
+			pass
 
-			while (True):
+		elif (diff < 0):
 
-				ir1 = rover.pingIR1()
+			rover.moveRover("cfr")
 
-				if (ir1):
+		elif (diff > 0):
 
-					break
-
-		else:
-
-			rover.moveRover("cfr", throttle = 0.5)
+			rover.moveRover("cfl")
 
 # Intersections are binary
 # stop = [0,0]
@@ -257,20 +200,3 @@ def obstacleAvoidance2_Strafe (rover, numObstacles):
 		if (count == numObstacles):
 
 			break
-
-# testing required
-# for type use: 0 = unknown/automonous 1 = stop 2 = forward 3 = right 4 = left
-# Need code for detecting the end of the lane (camera sensor analysis)
-def intersectionTest (rover, type):
-	rover.moveRover("f")
-	if type == 1:
-		rover.moveRover("s")
-	elif type == 2:
-		rover.moveRover("f")
-	elif type == 3:
-		rover.moverRover("cfr")
-	elif type == 4:
-		rover.moveRover("cfl")
-	else:
-		pass
-		# Insert Binary Protocol
