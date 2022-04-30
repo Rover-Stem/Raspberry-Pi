1682f7f3d0355e9f563db9ea461228fb515522bb
Zade Micallef <77754146+whoami-anvil@users.noreply.github.com>
2022-04-30 12:15:44 -0400

    20

--- presets.py
+++ presets.py
@@ -109,7 +109,7 @@ def obstacleAvoidance2 (rover, numObstacles = 1):
 		angle = 0
 		countUp = True
 
-		while ((distance > 40) or (distance < 0)):
+		while ((distance > 20) or (distance < 0)):
 
 			print(f"Distance {distance}")
 
