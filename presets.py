b6caf512cc0cd6daa8da339f8151ffd029805fcb
Geogre177 <mittry20017@gmail.com>
2022-04-30 11:01:20 -0400

    fix

--- presets.py
+++ presets.py
@@ -79,7 +79,7 @@ def directionChallenge (rover, target):
 	if (target > 0):
 
 		rover.moveRover("rr")
-		time.sleep(target / 131)
+		time.sleep(target / 150)
 		rover.moveRover("f")
 		time.sleep(3.2)
 		rover.moveRover("s")
@@ -87,7 +87,7 @@ def directionChallenge (rover, target):
 	else:
 
 		rover.moveRover("rl")
-		time.sleep(abs(target) /131)
+		time.sleep(abs(target) /150)
 		rover.moveRover("f")
 		time.sleep(3.2)
 		rover.moveRover("s")
