21e6b9bec1a9c72ebdfa2b5cb1476d548df307a7
Zade Micallef <77754146+whoami-anvil@users.noreply.github.com>
2022-04-30 11:57:40 -0400

    Turned up dist

--- presets.py
+++ presets.py
@@ -34,7 +34,7 @@ def obstacleAvoidance1 (rover):
 	angle = 0
 	countUp = True
 
-	while ((rover.measureDistance() > 10) or (rover.measureDistance() < 0)):
+	while ((rover.measureDistance() > 13) or (rover.measureDistance() < 0)):
 
 		if (countUp):
 
